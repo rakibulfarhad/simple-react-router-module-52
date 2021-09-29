c509590806584a84a65f7eabbfe923e419529d1e
rakibulfarhad <mraisfa@gmail.com>
2021-09-29 17:12:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './componants/Home/Home';
+import Friends from './componants/Friends/Friends';
+import About from './componants/About/About';
+import NotFound from './componants/NotFound/NotFound';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './componants/Header/Header';
+import FriendDetail from './componants/FriendDetail/FriendDetail';
+import Culture from './componants/Culture/Culture';
+import PostDetail from './componants/PostDetail/PostDetail';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Switch>
+        <Route exact path="/">
+            <Home></Home>
+          </Route>
+        <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/friends">
+            <Friends></Friends>
+          </Route>
+          <Route path="/friend/:friendId">
+            <FriendDetail></FriendDetail>
+          </Route>
+          <Route path="/post/:postId">
+            <PostDetail></PostDetail>
+          </Route>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+          <Route exact path="/about/culture">
+            <Culture></Culture>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
